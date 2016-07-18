12dc525f80a1710a03d208b6e6a70946b9cf4bfa
juanEs0304 <juanka0304@gmail.com>
2016-07-18 10:29:20 -0500

    Codigo Archivostxt

---
+++ Tarea2/Tarea2/LeeArchivo.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Tarea2
+{
+    class LeeArchivo
+    {
+
+
+        static void Main()
+        {
+
+
+            String line;
+            string[] frases;
+            try
+            {
+                
+                StreamReader sr = new StreamReader("frases.txt");
+                Console.WriteLine("Ingrese algunas frases");
+               
+                // Lee los valores almacenados en el archivo
+                line = sr.ReadLine();
+
+
+                while (line != null)
+                {
+             
+                    Console.WriteLine(line);
+                    line = sr.ReadLine();
+                }
+
+ 
+                sr.Close();
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+
+        }
+
+
+    }
+}
